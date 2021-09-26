676e59315fa7759375347e28d9b8deaab98ed7b2
nurakmaljalil91 <nurakmaljalil91@gmail.com>
2021-09-26 21:47:17 +0800

    Add test

--- src/tests/tests.rs
+++
@@ -1,9 +0,0 @@
-use rocket::local::blocking::Client;
-use rocket::http::{RawStr, Status};
-
-#[test]
-fn hello_world(){
-    let client = Client::tracked(super::rocket()).unwrap();
-    let response = client.get("/api").dispatch();
-    assert_eq!(response.into_string().unwrap(), expectde);
-}
